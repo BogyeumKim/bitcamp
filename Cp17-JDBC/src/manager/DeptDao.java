49de7a5280d13cda6bd1bab22e01205f3326f120
BogyeumKim <54786369+BogyeumKim@users.noreply.github.com>
2020-06-01 18:28:05 +0900

    jdbc

---
+++ Cp17-JDBC/src/manager/DeptDao.java
@@ -0,0 +1,16 @@
+package manager;
+
+public class DeptDao {
+
+	
+	// DAO = DAta Acess Object
+	// 데이터베이스 처리 하는 클래스 
+	//
+	
+	
+	
+	//MVC -> model, view , controller
+	// model -> service , dao
+	// 데이터베이스
+	
+}
---
+++ Cp17-JDBC/src/manager/Emp.java
@@ -0,0 +1,23 @@
+package manager;
+
+import java.sql.Date;
+
+
+
+// VO
+public class Emp {
+
+	
+	
+		private int empno;
+		private String ename;
+		private String job;
+		private int mgr;
+		private Date hiredate;
+		private int sal;
+		private int comm;
+		private int deptno;
+		
+		
+		
+}
